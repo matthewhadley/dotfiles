95d30088aa45f87f03eda6a01abbb51f913a5612
diffsky <matthew.hadley@gmail.com>
2013-09-29 18:01:51 -0700

    uncomment lines

--- init.sh
+++ init.sh
@@ -30,50 +30,41 @@ do :
   ln -sf "$f" "$HOME/.$file"
 done
 
-# for i in $HOME/.dotfiles/dotfiles/*
-# do :
-#   FILE=`basename "$i"`
-#   LINK="$i"
-#   echo $(basename $FILE)
+# ssh
+mkdir -p $HOME/.ssh
+cp $HOME/.dotfiles/ssh/config* $HOME/.ssh
 
-#   ln -sf "$LINK" "$HOME/.$FILE"
-# done
+if [ "$DOMAIN" = "osx" ];then
+  # setup mutt
+  rm -f $HOME/.mutt 2> /dev/null
+  ln -s $HOME/.dotfiles/mutt $HOME/.mutt
+fi
 
-# # ssh
-# mkdir -p $HOME/.ssh
-# cp $HOME/.dotfiles/ssh/config* $HOME/.ssh
+# Marks
+mkdir -p $HOME/.marks
 
-# if [ "$DOMAIN" = "osx" ];then
-#   # setup mutt
-#   rm -f $HOME/.mutt 2> /dev/null
-#   ln -s $HOME/.dotfiles/mutt $HOME/.mutt
-# fi
+# vim
+rm -rf $HOME/.vim
+ln -s $HOME/.dotfiles/vim $HOME/.vim
 
-# # Marks
-# mkdir -p $HOME/.marks
+# warn about bash-completion package not installed
+bash_warn="warn: package bash completion not present"
+if [[ "$DOMAIN" = "osx" && ! -f /usr/local/etc/bash_completion ]]; then
+  echo $bash_warn
+fi
+if [[ "$DOMAIN" = "centos" && ! -f /etc/bash_completion ]]; then
+  echo $bash_warn
+fi
 
-# # vim
-# rm -rf $HOME/.vim
-# ln -s $HOME/.dotfiles/vim $HOME/.vim
+# symlinks
+if [ "$DOMAIN" = "osx" ];then
+  ln -s $HOME/dev/python/keychain/keychain.py /usr/local/bin/keychain &> /dev/null
+fi
 
-# # warn about bash-completion package not installed
-# bash_warn="warn: package bash completion not present"
-# if [[ "$DOMAIN" = "osx" && ! -f /usr/local/etc/bash_completion ]]; then
-#   echo $bash_warn
-# fi
-# if [[ "$DOMAIN" = "centos" && ! -f /etc/bash_completion ]]; then
-#   echo $bash_warn
-# fi
+echo ".dotfiles init'd"
 
-# # symlinks
-# if [ "$DOMAIN" = "osx" ];then
-#   ln -s $HOME/dev/python/keychain/keychain.py /usr/local/bin/keychain &> /dev/null
-# fi
-
-# echo ".dotfiles init'd"
-
-# # source all the bash
-# source $HOME/.dotfiles/dotfiles/bashrc
-# echo "sourced .dotfiles"
+# source all the bash
+source $HOME/.dotfiles/dotfiles/bashrc
+echo "sourced .dotfiles"
 
 exit 0
