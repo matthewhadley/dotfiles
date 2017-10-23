e4015e669a0e87d49800f8081af93c6304cec957
diffsky <matthew.hadley@gmail.com>
2017-10-23 09:15:12 -0700

    cross platform friendly ssh-add

--- bashrc.d/platform/osx/00.env.bash
+++ bashrc.d/platform/osx/00.env.bash
@@ -5,3 +5,6 @@ export NODE_ENV=dev
 # https://github.com/diffsky/git-ssh
 export GIT_SSH=$HOME/dev/bash/git-ssh/git-ssh.sh
 export GIT_CORP=$HOME/dev/bash/git.corp/git.corp.sh
+
+# Add default ssh key
+ssh-add $HOME/.ssh/id_rsa 2>/dev/null
