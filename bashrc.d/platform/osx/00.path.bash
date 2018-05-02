53c78aee034d2fc1ae5e13272c0e61095d129b2e
diffsky <matthew.hadley@gmail.com>
2018-05-02 14:37:35 -0700

    add vue syntax highlighting for vim

---
+++ bashrc.d/platform/osx/00.path.bash
@@ -0,0 +1,6 @@
+# This formula is keg-only, which means it was not symlinked into /usr/local,
+# because this is an alternate version of another formula.
+
+# If you need to have this software first in your PATH run:
+#   echo 'export PATH="/usr/local/opt/node@8/bin:$PATH"' >> ~/.bash_profile
+pathadd "/usr/local/opt/node@8/bin"
