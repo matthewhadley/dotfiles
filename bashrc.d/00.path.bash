a9144831dd0d01ef69172260d87bdafe9f05a17f
matthewhadley <matthew.hadley@gmail.com>
2020-05-09 21:17:00 -0700

    move homebrew path requirements under OSX plartform

--- bashrc.d/00.path.bash
+++ bashrc.d/00.path.bash
@@ -7,8 +7,6 @@ function pathadd {
 
 pathadd "/usr/local/sbin"
 pathadd "/usr/local/bin"
-pathadd "/usr/local/opt/node@12/bin"
-pathadd "/usr/local/opt/openjdk/bin"
 
 # Note, edit /etc/paths to put local paths before global
 # http://stackoverflow.com/questions/5364614/
