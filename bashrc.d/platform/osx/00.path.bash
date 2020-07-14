855ac6171a216fa511955fa609b17fa22ed7cbeb
matthewhadley <matthew.hadley@gmail.com>
2020-07-14 10:16:30 -0500

    use node 14

--- bashrc.d/platform/osx/00.path.bash
+++ bashrc.d/platform/osx/00.path.bash
@@ -1,5 +1,5 @@
 # This formula is keg-only, which means it was not symlinked into /usr/local,
 # because this is an alternate version of another formula.
 
-pathadd "/usr/local/opt/node@12/bin"
+pathadd "/usr/local/opt/node@14/bin"
 pathadd "/usr/local/opt/openjdk/bin"
