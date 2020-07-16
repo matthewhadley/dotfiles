705c0c44b20ef935690e2e159a282394fcc3543d
matthewhadley <matthew.hadley@gmail.com>
2020-07-16 13:54:49 -0500

    fix iterm tab prompt

--- zshrc.d/00.prompt.zsh
+++ zshrc.d/00.prompt.zsh
@@ -8,43 +8,35 @@ GIT_PS1_SHOWDIRTYSTATE=1
 GIT_PS1_SHOWUNTRACKEDFILES=1
 source $(brew --prefix)/etc/bash_completion.d/git-prompt.sh
 
-# show 2 levels of directory listing for iterm2 tab
-# check not running on local console
-if [ "$(tty | grep pts | wc -l)" -ge "1" ];then
-  DEV_PTS=1
-fi
-
 two_dirs() {
-  if [[ "$PLATFORM" == "osx" || -n "$DEV_PTS" ]];then
-    local home_sign="~" # rootbashrc uses "/root"
-    local dir="${PWD%/*/*}"
-    local last
-    if [ "${#dir}" -gt 0 -a "$dir" != "$PWD" ];then
-      last="${PWD:${#dir}+1}"
-    else
-      last="$PWD";
-    fi
-    if [[ "$last" == "$HOME" ]];then
-      last="$home_sign"
-    fi
-    if [[ "$(dirname "$last")" == "$USER" ]];then
-      last="$home_sign/${PWD##*/}"
-    else
-      if [[ "$(echo "$last" | cut -c 1)" != "/" && "$last" != "$home_sign" ]];then
-        last="$last" # or ../$last if you want to indicate that at depth
-      fi
-    fi
-    # show hostname if in ssh session
-    if [[ -n "$SSH_CLIENT" ]]; then
-      local hostname="[$HOSTNAME] "
-    fi
-    if [[ ! -z "TTY_NUM" ]]; then
-      local tty_display=" $TTY_NUM "
-    fi
-
-    # set iterm tab display
-    echo -ne "\033]0;${tty_display}${hostname}${last}\007"
+  # show 2 levels of directory listing for iterm2 tab
+  local dir="${PWD%/*/*}"
+  local last
+  if [ "${#dir}" -gt 0 -a "$dir" != "$PWD" ];then
+    last="${PWD:${#dir}+1}"
+  else
+    last="$PWD";
   fi
+  if [[ "$last" == "$HOME" ]];then
+    last="~"
+  fi
+  if [[ "$(dirname "$last")" == "$USER" ]];then
+    last="~/${PWD##*/}"
+  else
+    if [[ "$(echo "$last" | cut -c 1)" != "/" && "$last" != "~" ]];then
+      last="$last" # or ../$last if you want to indicate that at depth
+    fi
+  fi
+  # show hostname if in ssh session
+  if [[ -n "$SSH_CLIENT" ]]; then
+    local hostname="[$HOSTNAME] "
+  fi
+  if [[ ! -z "TTY_NUM" ]]; then
+    local tty_display=" $TTY_NUM "
+  fi
+
+  # set iterm tab display
+  echo -ne "\033]0;${tty_display}${hostname}${last}\007"
 }
 
 prompt_command() {
