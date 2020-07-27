a0185e3bb033708d30c81246d0e9f770835b824a
matthewhadley <matthew.hadley@gmail.com>
2020-07-27 11:05:53 -0500

    case insensitive tab completion

--- zshrc.d/01.completion.zsh
+++ zshrc.d/01.completion.zsh
@@ -8,3 +8,7 @@ if [[ -d $(brew --prefix)/etc/bash_completion.d/ ]]; then
   zstyle ':completion:*:*:git:*' script /usr/local/etc/bash_completion.d/git-completion.bash
   fpath=(~/usr/local/etc/bash_completion.d/ $fpath)
 fi
+
+# superuser.com/questions/1092033/how-can-i-make-zsh-tab-completion-fix-capitalization-errors-for-directories-and/1092328
+# tab completion capital letters also match small letters
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
