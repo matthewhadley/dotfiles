6d3443d4f31749ca29742ab29ff39c681b2cdae3
matthewhadley <matthew.hadley@gmail.com>
2022-01-12 15:13:11 -0600

    update git alias

--- zshrc.d/02.git-aliases.zsh
+++ zshrc.d/02.git-aliases.zsh
@@ -1,7 +1,7 @@
 # channel git calls
 hash git 2>&- && { export GIT_PATH=$(which git); }
 
-git(){
+function git(){
   if [[ "$1" == "commit" || "$1" == "clone" ]];then
     # let git.corp proxy git cmd
     $GIT_CORP "$@"
