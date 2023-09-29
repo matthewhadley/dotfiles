b38e8c84a50190c54b40c079e310bb4e95a06ea6
matthewhadley <matthew.hadley@gmail.com>
2023-09-29 16:58:35 -0500

    cleanup

--- zshrc.d/02.aliases.zsh
+++ zshrc.d/02.aliases.zsh
@@ -8,22 +8,11 @@ alias vi=vim
 # Recursively delete `.DS_Store` files
 alias rm.DS="find . -name '*.DS_Store' -type f -ls -delete"
 
-# sublime text
-alias sb='/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'
-alias sbg='/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl `git diff --name-only | tr "\n" " "`'
-alias sbn='/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl -n .'
-alias sbng='/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl -n . `git diff --name-only | tr "\n" " "`'
-
 # ripgrep
-alias rg="rg --colors 'match:bg:yellow' --colors 'match:fg:black' --colors 'line:fg:magenta'"
+alias rg="rg --colors 'match:bg:yellow' --colors 'match:fg:black' --colors 'line:fg:white'"
 
 # npm
 alias npm-public='npm --registry https://registry.npmjs.org'
-
-# reconnect that Magic Mouse
-btcc() {
-  btc $(btc | grep "Magic Mouse" | cut -c-17)
-}
 
 # local webserver github.com/http-party/http-server
 # localhost only without caching
