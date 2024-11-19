f7b93d9e68f515e2291e851736c83207628ae15d
matthewhadley <matthew.hadley@gmail.com>
2024-11-18 22:14:53 -0600

    fix git autocomplete

--- zshrc.d/01.completion.zsh
+++ zshrc.d/01.completion.zsh
@@ -4,10 +4,7 @@ autoload -U compinit
 compinit
 
 # brew bash-completion package
-if [[ -d $(brew --prefix)/etc/bash_completion.d/ ]]; then
-  zstyle ':completion:*:*:git:*' script /usr/local/etc/bash_completion.d/git-completion.bash
-  fpath=(~/usr/local/etc/bash_completion.d/ $fpath)
-fi
+[[ -r "/opt/homebrew/etc/profile.d/bash_completion.sh" ]] && . "/opt/homebrew/etc/profile.d/bash_completion.sh"
 
 # superuser.com/questions/1092033/how-can-i-make-zsh-tab-completion-fix-capitalization-errors-for-directories-and/1092328
 # tab completion capital letters also match small letters
